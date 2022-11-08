c785047f2a796e8dfe19ddbd930833b8cdbd999b
Ryan Block <rblock@gmail.com>
2022-11-08 10:44:56 -0800

    an attempt was made

--- index.js
+++ index.js
@@ -1,34 +1,62 @@
 let { existsSync, mkdirSync, rmSync, writeFileSync } = require('fs')
+let https = require('https')
 let { join } = require('path')
 let dir = join(__dirname, 'folder')
 let file = join(dir, 'hi.txt')
 
-if (existsSync(dir)) {
-  console.log(`please ensure folder (and contents) do not exist before running this test: ${dir}`)
-  process.exit(1)
+async function main () {
+  if (existsSync(dir)) {
+    console.log(`please ensure folder (and contents) do not exist before running this test: ${dir}`)
+    process.exit(1)
+  }
+
+  mkdirSync(dir, { recursive: true })
+  writeFileSync(file, 'hi!')
+
+  let result = await new Promise((resolve, reject) => {
+    let url = 'https://google.com'
+    https.get(url, res => {
+      let body = []
+      res.on('data', data => {
+        body.push(data)
+      })
+      res.on('end', data => {
+        if (data) body.push(data)
+        body = Buffer.concat(body).toString()
+        console.log(body.substr(0,25))
+        resolve('Loaded the googles!')
+      })
+      res.on('err', err => {
+        reject(err)
+      })
+    })
+  })
+
+  if (!existsSync(file)) {
+    console.log(`something went wrong setting things up: ${file}`)
+    process.exit(1)
+  }
+
+  try {
+    rmSync(dir, { recursive: true, force: true })
+  }
+  catch (err) {
+    console.log(`error during rm: ${dir}`)
+    console.log(err)
+    process.exit(1)
+  }
+
+  if (existsSync(dir)) {
+    console.log(`node failed to rm: ${dir}`)
+    process.exit(1)
+  }
+  else {
+    console.log(`rm success: ${dir}`)
+  }
 }
 
-mkdirSync(dir, { recursive: true })
-writeFileSync(file, 'hi!')
-
-if (!existsSync(file)) {
-  console.log(`something went wrong setting things up: ${file}`)
-  process.exit(1)
+async function go () {
+  await main()
+  await main()
 }
-
-try {
-  rmSync(dir, { recursive: true, force: true })
-}
-catch (err) {
-  console.log(`error during rm: ${dir}`)
-  console.log(err)
-  process.exit(1)
-}
-
-if (existsSync(dir)) {
-  console.log(`node failed to rm: ${dir}`)
-  process.exit(1)
-}
-else {
-  console.log(`rm success: ${dir}`)
-}
+go()
